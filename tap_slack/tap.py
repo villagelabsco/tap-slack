2c2ce16a67448933ede081dc3fea1c71096c4f03
regan-village <regan@villagelabs.co>
2025-08-28 13:09:44 -0500

    Update tap slack to always perform required auto join actions (#15)
    * First commit
    
    * comment
    
    * more
    
    ---------
    
    Co-authored-by: Regan Loper <regan.c.loper@gmail.com>

--- tap_slack/tap.py
+++ tap_slack/tap.py
@@ -9,24 +9,27 @@ from singer_sdk import typing as th
 
 from tap_slack.streams import (
     ChannelsStream,
-    ChannelMembersStream,
-    MessagesStream,
-    ThreadsStream,
-    UsersStream,
-    MessageReactionsStream,
-    ThreadReactionsStream,
+    # ChannelMembersStream,
+    # MessagesStream,
+    # ThreadsStream,
+    # UsersStream,
+    # MessageReactionsStream,
+    # ThreadReactionsStream,
 )
 
+# TODO - turn all of these streams back on (except ChannelMembers) when our Slack app is 
+# approved. Right now we just need ChannelsStream to run and auto-join public channels
+# via the post_process method
 STREAM_TYPES = [
     ChannelsStream,
     # For now, disable the stream of members included in a channel
     # It is not very useful information, and not used by Argo reports
     # ChannelMembersStream,
-    MessagesStream,
-    ThreadsStream,
-    UsersStream,
-    MessageReactionsStream,
-    ThreadReactionsStream,
+    # MessagesStream,
+    # ThreadsStream,
+    # UsersStream,
+    # MessageReactionsStream,
+    # ThreadReactionsStream,
 ]
 ADMIN_STREAM_TYPES = []
 
